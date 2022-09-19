d8a304daedb68f75b521904776f8f8941b0018c3
onury5506 <onuryildizsai@gmail.com>
2022-09-19 19:34:12 +0300

    EthWalletBalanceCheck

--- index.js
+++ index.js
@@ -6,8 +6,46 @@ const port = 8080
 
 app.use(express.json())
 
-app.post("/EthWalletCheck",(req,res)=>{
-    res.send(process.env.TEST)
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER))
+
+app.post("/EthWalletBalanceCheck",async (req,res)=>{
+    let wallets = req.body.wallets
+    if(!Array.isArray(wallets)){
+        return res.send({
+            error:"Invalid Input!"
+        })
+    }
+    let response = []
+    let promises = []
+
+    wallets.forEach((wallet,i)=>{
+
+        let walletBalance = {
+            wallet,
+            error:false
+        }
+
+        response[i] = walletBalance
+
+        if(web3.utils.isAddress(wallet)){
+            let promise = web3.eth.getBalance(wallet,(err,balance)=>{
+                if(err){
+                    walletBalance.error=true
+                    return walletBalance.errorMsg = err
+                }
+                walletBalance.balance = balance
+            })
+            promises.push(promise)
+        }else{
+            walletBalance.error=true
+            walletBalance.errorMsg = "Wallet is not valid!"
+        }
+    })
+
+    await Promise.all(promises)
+
+    res.send(response)
 })
 
 app.listen(port, () => {
